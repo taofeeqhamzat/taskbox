244502fdfe203010054dc45e394d948abb2247ec
Tao. H <hamzatdestiny@gmail.com>
2022-11-29 23:04:53 +0100

    refactor: Move mocked app state to reuseable module

--- src/components/TaskList/TaskList.stories.js
+++ src/components/TaskList/TaskList.stories.js
@@ -1,50 +1,11 @@
 import React from "react";
 
 import TaskList from "./TaskList";
-import * as TaskStories from "../Task/Task.stories";
+import { Mockstore, MockedState } from './util/util';
 
-import { Provider } from "react-redux";
+// A super-simple mock of the state of the store, "MockedState" imported from module
+// A super-simple mock of a redux store, "Mockestore" imported from module
 
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-// A super-simple mock of the state of the store
-export const MockedState = {
-  tasks: [
-    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
-    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
-    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
-    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
-    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
-    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
-  ],
-  status: "idle",
-  error: null,
-};
-
-// A super-simple mock of a redux store
-const Mockstore = ({ taskboxState, children }) => (
-  <Provider
-    store={configureStore({
-      reducer: {
-        taskbox: createSlice({
-          name: "taskbox",
-          initialState: taskboxState,
-          reducers: {
-            updateTaskState: (state, action) => {
-              const { id, newTaskState } = action.payload;
-              const task = state.tasks.findIndex((task) => task.id === id);
-              if (task >= 0) {
-                state.tasks[task].state = newTaskState;
-              }
-            },
-          },
-        }).reducer,
-      },
-    })}
-  >
-    {children}
-  </Provider>
-);
 
 export default {
   component: TaskList,
