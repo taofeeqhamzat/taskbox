f61b715a43646b0b01eb16d66237012e92d2bc36
Tao. H <hamzatdestiny@gmail.com>
2022-12-01 02:16:54 +0100

    refactor: Use custom render wrapper for tests

--- src/components/TaskList/util/util.js
+++ src/components/TaskList/util/util.js
@@ -1,12 +1,13 @@
 import { Provider } from "react-redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import PropTypes from "prop-types";
+import { render } from "@testing-library/react";
 
 import Task from "../../Task/Task";
 import * as TaskStories from "../../Task/Task.stories";
 
 // A super-simple mock of the state of the store
-export const MockedState = {
+const MockedState = {
   tasks: [
     { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
     { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
@@ -20,7 +21,7 @@ export const MockedState = {
 };
 
 // A super-simple mock of a redux store
-export const Mockstore = ({ taskboxState, children }) => (
+const Mockstore = ({ taskboxState, children }) => (
   <Provider
     store={configureStore({
       reducer: {
@@ -44,16 +45,25 @@ export const Mockstore = ({ taskboxState, children }) => (
   </Provider>
 );
 
-export const pinnedtasks = [
+Mockstore.propTypes = {
+  taskboxState: PropTypes.shape({
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+    status: PropTypes.string,
+    error: PropTypes.any,
+  }),
+};
+
+const pinnedtasks = [
   ...MockedState.tasks.slice(1, 5),
   { id: "1", title: "Task 1 (pinned)", state: "TASK_PINNED" },
   { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
 ];
 
-Mockstore.propTypes = {
-  taskboxState: PropTypes.shape({
-    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-    status: PropTypes.string,
-    error: PropTypes.any
-  })
+const wrapRender = (ui, taskboxState, options) => {
+  const StoreWrapper =({children})=> <Mockstore taskboxState={taskboxState}>{children}</Mockstore>;
+  return render(ui, { wrapper: StoreWrapper, ...options });
 };
+
+// Export modified render function along with the rest of the testing library
+export { MockedState, Mockstore, pinnedtasks, wrapRender as render };
+export * from "@testing-library/react";
