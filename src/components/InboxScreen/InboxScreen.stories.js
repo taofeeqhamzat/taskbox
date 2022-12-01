960553fe62121b8ba5870d0d03596ac5060c5e91
Tao. H <hamzatdestiny@gmail.com>
2022-12-01 04:41:17 +0100

    feat: Add storybook interactions

--- src/components/InboxScreen/InboxScreen.stories.js
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -1,10 +1,17 @@
 import InboxScreen from "./InboxScreen";
 import store from "../../lib/store";
 
-import {MockedState} from "../../lib/util/util"
+import { MockedState } from "../../lib/util/util";
 import { rest } from "msw";
 
 import { Provider } from "react-redux";
+
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@storybook/testing-library";
 
 export default {
   component: InboxScreen,
@@ -19,7 +26,7 @@ Default.parameters = {
   msw: {
     handlers: [
       rest.get(
-        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
         (req, res, ctx) => {
           return res(ctx.json(MockedState.tasks));
         }
@@ -28,16 +35,29 @@ Default.parameters = {
   },
 };
 
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
+};
+
 export const Error = Template.bind({});
 Error.parameters = {
-    msw: {
-      handlers: [
-        rest.get(
-          'https://jsonplaceholder.typicode.com/todos?userId=1',
-          (req, res, ctx) => {
-            return res(ctx.status(403));
-          }
-        ),
-      ],
-    },
-  };
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
